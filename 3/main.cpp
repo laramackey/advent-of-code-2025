d31dbdc17c757538200b365381af022df27f2bd5
laramackey <lara.mackey@cse.org.uk>
2025-12-03 23:26:40 +0000

    day 3 task 2
    stacks! $

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,43 +2,48 @@
 // Created by lara on 12/3/25.
 //
 
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 
-int joltage(std::ifstream &file)
+
+unsigned long long joltage(std::ifstream &file, int number_of_digits)
 {
+    file.clear();
+    file.seekg(0);
     std::string line;
-    int total = 0;
+    unsigned long long total = 0;
 
     while (std::getline(file, line))
     {
-        int largest_index = 0;
-        int largest_value = line[0] - '0';
-        for (int i = 1; i < line.size() - 1; i++) {
-            if (const int digit = line[i] - '0'; digit > largest_value) {
-                largest_value = digit;
-                largest_index = i;
-            }
-        }
-        int second_largest_value = line[largest_index + 1] - '0';
-        for (int i = largest_index + 2; i < line.size(); i++) {
-            if (const int digit = line[i] - '0'; digit > second_largest_value) {
-                second_largest_value = digit;
-            }
-        }
-        const int combined = largest_value * 10 + second_largest_value;
-        total += combined;
-    }
+        std::vector<char> stack;
+        const int line_size = line.size();
 
+        for (int i = 0; i < line_size; i++)
+        {
+            char c = line[i];
+            while (!stack.empty() && c > stack.back() && (stack.size() - 1 + line_size - i) >= number_of_digits)
+            {
+                stack.pop_back();
+            }
+            if (stack.size() < number_of_digits)
+            {
+                stack.push_back(c);
+            }
+        }
+        auto largest12 = std::string(stack.begin(), stack.end());
+        total += std::stoull(largest12);
+    }
     return total;
 }
 
 int main()
 {
     std::ifstream file("input.txt");
-    const int j = joltage(file);
-    std::cout << "Max joltage: " << j << "\n";
-
+    const long long task1 = joltage(file, 2);
+    const long long task2 = joltage(file, 12);
+    std::cout << "Max joltage 1: " << task1 << "\n";
+    std::cout << "Max joltage 2: " << task2 << "\n";
     return 0;
 }
