7f7060c386d916758feb0453be7baf85e4a5411b
laramackey <lara.mackey@cse.org.uk>
2025-12-04 22:38:58 +0000

    day 4 part 2

--- 4/main.cpp
+++ 4/main.cpp
@@ -51,28 +51,51 @@ bool is_accessible(std::vector<std::vector<int>> grid, int x, int y)
     return true;
 }
 
-int find_paper_rolls(std::ifstream &file)
-{
-    file.clear();
-    file.seekg(0);
+struct Result {
+    int accessible_count;
+    std::vector<std::vector<int>> grid;
+};
 
-    auto grid = file_to_grid(file);
+Result find_paper_rolls(std::vector<std::vector<int>>grid, int task)
+{
     int accessible_count = 0;
 
     for (int y = 0; y < grid.size(); ++y)
     {
         for (int x = 0; x < grid[y].size(); ++x)
         {
-            if (grid[y][x] == 1) accessible_count += (is_accessible(grid, x, y) ? 1 : 0);
+            if (grid[y][x] == 1 && (is_accessible(grid, x, y)))
+            {
+                accessible_count++;
+                if (task == 2) grid[y][x] = 0;
+            }
         }
     }
-    return accessible_count;
+    return { accessible_count, grid };
+}
+
+Result find_paper_rolls_and_remove(const std::vector<std::vector<int>>&grid)
+{
+    int accessible_count = 0;
+    bool found_more = true;
+    std::vector<std::vector<int>>current_grid = grid;
+    while (found_more)
+    {
+        auto result = find_paper_rolls(current_grid, 2);
+        current_grid = result.grid;
+        if (result.accessible_count == 0) found_more = false;
+        else accessible_count += result.accessible_count;
+    }
+    return { accessible_count, grid };
 }
 
 int main()
 {
     std::ifstream file("input.txt");
-    const int task1 = find_paper_rolls(file);
-    std::cout << "No. of paper rolls: " << task1 << "\n";
+    const auto grid = file_to_grid(file);
+    const auto task1 = find_paper_rolls(grid, 1);
+    std::cout << "No. of paper rolls task 1: " << task1.accessible_count << "\n";
+    const auto task2 = find_paper_rolls_and_remove(grid);
+    std::cout << "No. of paper rolls task 2: " << task2.accessible_count << "\n";
     return 0;
 }
