87e32346309edbeae609915dd1acaffb0072c6cd
laramackey <lara.mackey@cse.org.uk>
2025-12-04 23:21:45 +0000

    day 4 reduce grid size calculations

--- 4/main.cpp
+++ 4/main.cpp
@@ -25,7 +25,7 @@ std::vector<std::vector<int>> file_to_grid(std::ifstream &file)
     return grid;
 }
 
-bool is_accessible(const std::vector<std::vector<int>> &grid, size_t x, size_t y)
+bool is_accessible(const std::vector<std::vector<int>> &grid, int x, int y, int x_length, int y_length)
 {
     int count = 0;
     for (int delta_x = -1; delta_x <= 1; ++delta_x)
@@ -33,16 +33,12 @@ bool is_accessible(const std::vector<std::vector<int>> &grid, size_t x, size_t y
         for (int delta_y = -1; delta_y <= 1; ++delta_y)
         {
             if (delta_x == 0 && delta_y == 0) continue;
-            int new_x = static_cast<int>(x) + delta_x;
-            int new_y = static_cast<int>(y) + delta_y;
-            if (new_y >= 0 && new_y < grid.size() &&
-                new_x >= 0 && new_x < grid[new_y].size())
+            int new_x = x + delta_x;
+            int new_y = y + delta_y;
+            if (new_y >= 0 && new_y < y_length &&
+                new_x >= 0 && new_x < x_length)
             {
-                if (grid[new_y][new_x] == 1)
-                {
-                    count++;
-                    if (count == 4) return false;
-                }
+                if (grid[new_y][new_x] == 1 && ++count == 4) return false;
             }
         }
     }
@@ -57,11 +53,13 @@ struct Result {
 Result find_paper_rolls(std::vector<std::vector<int>> &grid, int task)
 {
     int accessible_count = 0;
-    for (size_t y = 0; y < grid.size(); ++y)
+    int y_length = grid.size();
+    int x_length = grid[0].size();
+    for (int y = 0; y < y_length; ++y)
     {
-        for (size_t x = 0; x < grid[y].size(); ++x)
+        for (int x = 0; x < x_length; ++x)
         {
-            if (grid[y][x] == 1 && is_accessible(grid, x, y))
+            if (grid[y][x] == 1 && is_accessible(grid, x, y, x_length, y_length))
             {
                 accessible_count++;
                 if (task == 2) grid[y][x] = 0;
