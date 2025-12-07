9cf09b84964e650690e2dbf38645ac1003e26356
laramackey <lara.mackey@cse.org.uk>
2025-12-07 23:49:57 +0000

    day 6 task 2 long long

--- 6/main.cpp
+++ 6/main.cpp
@@ -3,56 +3,76 @@
 //
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
 #include <string>
 
-std::string trim(const std::string &s) {
-    size_t start = s.find_first_not_of(' ');
-    size_t end   = s.find_last_not_of(' ');
-    if (start == std::string::npos) return "";
-    return s.substr(start, end - start + 1);
+struct ColumnParseResult
+{
+    std::vector<size_t> starts;
+    std::vector<char> operators;
+};
+
+ColumnParseResult find_column_starts_and_operators(const std::string& line)
+{
+    ColumnParseResult result;
+    for (size_t i = 0; i < line.size(); ++i)
+    {
+        if (line[i] == '*' || line[i] == '+')
+            result.operators.push_back(line[i]);
+
+        if (line[i] != ' ' && (i == 0 || line[i - 1] == ' '))
+            result.starts.push_back(i);
+    }
+    result.starts.push_back(line.size() + 2); //hack cause clion won't save file with trailing space
+    return result;
 }
 
-int main() {
+std::vector<std::string> split_by_columns(const std::string& line,
+                                        const std::vector<size_t>& starts)
+{
+    std::vector<std::string> out;
+    out.reserve(starts.size() - 1);
+    for (size_t i = 0; i + 1 < starts.size(); ++i)
+    {
+        out.push_back(line.substr(starts[i], starts[i + 1] - starts[i] - 1));
+    }
+    return out;
+}
+
+int main()
+{
     std::ifstream file("input.txt");
-    std::vector<std::vector<std::string>> columns;
+    std::vector<std::string> rows;
     std::string line;
 
-    while (std::getline(file, line))
-    {
-        std::vector<std::string> tokens;
-        std::string token;
-        std::stringstream ss(line);
+    // read file
+    while (std::getline(file, line)) rows.push_back(line);
 
-        while (ss >> token)
-        {
-            tokens.push_back(token);
-        }
+    // get column start points based on bottom row, and list of operator
+    auto starts_and_operators = find_column_starts_and_operators(rows.back());
+    std::vector<std::vector<std::string>> table;
+    // split columns by the start points calculated from the bottom row in order to retain spaces
+    for (auto& r : rows) table.push_back(split_by_columns(r, starts_and_operators.starts));
 
-        if (columns.empty()) columns.resize(tokens.size());
-
-        for (size_t i = 0; i < tokens.size(); ++i)
-        {
-            columns[i].push_back(tokens[i]);
-        }
-    }
 
     long long total = 0;
-    for (auto & v : columns)
+    for (int i = 0; i < table[0].size(); i++)
     {
-        char op = v.back()[0];
-        long long result = std::stoll(v[0]);
-        for (size_t i = 1; i < v.size() - 1; ++i)
+        char op = starts_and_operators.operators[i];
+        long long column_res = op == '+' ? 0 : 1;
+        //iterate through number right to left, if it's not a space, add it to number for that col index
+        for (int j = table[0][i].size() - 1; j >= 0; --j)
         {
-            long long num = std::stoll(v[i]);
-            if (op == '+') result += num;
-            else if (op == '*') result *= num;
+            std::string column_number;
+            // check col index for all 4 number rows
+            for (int k = 0; k < 4; k++)  if (table[k][i][j] != ' ') column_number += table[k][i][j];
+            if (op == '+') column_res += std::stoi(column_number);
+            else if (op == '*') column_res *= std::stoi(column_number);
         }
-        total += result;
+        total += column_res;
     }
 
-    std::cout << "Total: " << total;
+    std::cout << total << std::endl;
 
     return 0;
 }
